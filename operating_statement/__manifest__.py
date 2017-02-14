91ebec779a0d84e857cf5e831105611355f7ba46
tsengeagle <tsengeagle@gmail.com>
2017-02-14 11:40:21 +0800

    部門的view

--- operating_statement/__manifest__.py
+++ operating_statement/__manifest__.py
@@ -25,8 +25,8 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
         'views/templates.xml',
+        'views/department_view.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
