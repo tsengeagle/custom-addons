69b16bde760b206ffd59ca66202087a1a79c8382
tsengeagle <tsengeagle@gmail.com>
2017-02-14 10:49:18 +0800

    部門及會計科目的設定檔

--- operating_statement/__manifest__.py
+++ operating_statement/__manifest__.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "OperatingStatement",
+    'name': "Operating Statement",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        科經營報表系統
+        """,
 
     'description': """
-        Long description of module's purpose
+
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "tsengeagle",
+    'website': "http://www.tzuchi.com.tw",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Accounting',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -32,4 +32,5 @@
     'demo': [
         'demo/demo.xml',
     ],
+    'application':True,
 }
