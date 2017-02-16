546ea6ad18a00c1815b283e036c6d64d8d9fc959
tsengeagle <tsengeagle@gmail.com>
2017-02-16 10:38:22 +0800

    課程與選課資訊之間的關聯

--- open_academy/models/course.py
+++ open_academy/models/course.py
@@ -7,3 +7,4 @@ class Course(models.Model):
     _name = 'open_academy.course'
     name = fields.Char(string='課程名稱', size=10, required=True)
     description = fields.Text(string='描述', size=100)
+    responsible_id = fields.Many2one('res.users', ondelete='set null', string='負責人', index=True)
--- open_academy/models/session.py
+++ open_academy/models/session.py
@@ -10,3 +10,6 @@ class Session(models.Model):
     start_date = fields.Date()
     duration = fields.Float(digits=(6, 2), help='Duration in days')
     seats = fields.Integer(string='Number of seats')
+
+    instructor_id = fields.Many2one('res.partner', string='指導者')
+    course_id = fields.Many2one('open_academy.course', ondelete='cascade', string='課程', required=True)
